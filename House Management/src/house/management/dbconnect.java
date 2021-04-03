6333af07f7bdbcd15b0622b14ab11fdec25f76d2
Khondoker Fuad Kabir <kfk.aryan@gmail.com>
2021-04-03 11:31:12 +0600

    main

--- House Management/src/house/management/dbconnect.java
+++ House Management/src/house/management/dbconnect.java
@@ -8,7 +8,7 @@ public class dbconnect {
         Connection con = null;
       try{
           Class.forName("com.mysql.jdbc.Driver");
-          con = DriverManager.getConnection("jdbc:mysql://localhost/hms", "root", "");
+          con = DriverManager.getConnection("jdbc:mysql://localhost/hm", "root", "");
           return con;
       }catch (Exception ex){
         System.out.println("There are error");
