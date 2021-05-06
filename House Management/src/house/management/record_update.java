2508a92a5dd03feb7f0810a48060770c30020dae
Sadman Shafin <76437706+Sadman-Shafin@users.noreply.github.com>
2021-05-06 13:24:40 +0800

    main

--- House Management/src/house/management/record_update.java
+++ House Management/src/house/management/record_update.java
@@ -384,10 +384,71 @@ public class record_update extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void flat_searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_flat_searchActionPerformed
-        
+   String fl = flat.getText();
+
+        Connection dbcon = dbconnect.connectDB();
+         try {
+                
+            PreparedStatement st = (PreparedStatement)    
+            dbcon.prepareStatement("select * from add_tenter where flat=?");
+            st.setString(1, fl);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                name.setText(rs.getString("name"));
+                gender.setSelectedItem(rs.getString("gender"));
+                member.setText(rs.getString("member"));
+                contact.setText(rs.getString("contact"));
+                address.setText(rs.getString("address"));                
+                nid.setText(rs.getString("nid"));
+                months.setSelectedItem(rs.getString("month"));
+                             
+            } 
+                else{
+                       JOptionPane.showMessageDialog(null, "Not Found...!!!");        
+            }
+            } catch (SQLException ex) {
+        }        
     }//GEN-LAST:event_flat_searchActionPerformed
 
     private void updateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateActionPerformed
+Connection dbcon = dbconnect.connectDB();
+        
+        try {
+            PreparedStatement st = (PreparedStatement)    
+            dbcon.prepareStatement("update add_tenter set name =?, gender = ?, member =?,"
+                    + "contact =?, address =?, nid =?, month =? where flat =?");
+            
+            
+            
+            st.setString(1, name.getText());
+            st.setString(2, gender.getSelectedItem().toString()); 
+            st.setString(3, member.getText());
+            st.setString(4, contact.getText());
+            st.setString(5, address.getText());
+            st.setString(6, nid.getText());
+            st.setString(7, months.getSelectedItem().toString());
+            st.setString(8, flat.getText());
+            
+            
+            int rs = st.executeUpdate();
+            
+            JOptionPane.showMessageDialog(this, "Updated Successful.", 
+                    "Success", JOptionPane.INFORMATION_MESSAGE);
+            
+            name.setText("");
+            name.requestFocus();
+            gender.setSelectedItem("");
+            member.setText("");
+            contact.setText("");
+            address.setText("");
+            nid.setText("");
+            months.setSelectedItem("");
+            
+
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(record_update.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
 
